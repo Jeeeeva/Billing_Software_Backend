84224726b620c8b428d1070e087f1935b2bf3b19
Jeeeeva <jeeeeva456@gmail.com>
2025-07-23 17:31:16 +0530

    Made created a online db and merged with this

--- src/main/java/com/example/Billingsoftware/BillingsoftwareApplication.java
+++ src/main/java/com/example/Billingsoftware/BillingsoftwareApplication.java
@@ -3,7 +3,9 @@ package com.example.Billingsoftware;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+
 public class BillingsoftwareApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/Billingsoftware/config/AdminInitializer.java
+++ src/main/java/com/example/Billingsoftware/config/AdminInitializer.java
@@ -3,6 +3,7 @@ package com.example.Billingsoftware.config;
 import com.example.Billingsoftware.entity.UserEntity;
 import com.example.Billingsoftware.Respository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
@@ -11,19 +12,23 @@ import jakarta.annotation.PostConstruct;
 @Component
 @RequiredArgsConstructor
 public class AdminInitializer {
+    @Value("${ADMIN_EMAIL}")
+    private String email;
+    @Value("${ADMIN_PASSWORD}")
+    private String password;
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
     @PostConstruct
     public void initAdmin() {
-        String adminEmail = "admin@gmail.com";
+        String adminEmail = email;
         if (userRepository.findByEmail(adminEmail).isEmpty()) {
             UserEntity admin = UserEntity.builder()
                     .userId(java.util.UUID.randomUUID().toString())
                     .name("Admin")
                     .email(adminEmail)
-                    .password(passwordEncoder.encode("admin123"))
+                    .password(passwordEncoder.encode(password))
                     .role("ROLE_ADMIN")
                     .build();
 
